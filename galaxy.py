e7f644b0b4df86f06213c34ed9e17fe93d92ec09
David Carton <carton@strw.leidenuniv.nl>
2014-11-10 17:44:02 +0100

    add reshift to Galaxy class

--- galaxy.py
+++ galaxy.py
@@ -1,12 +1,50 @@
+import numpy as np
+
 class Galaxy(object):
     """Galaxy model producing undegraded 2D emission line maps"""
-    def __init__(self, z, R_e):
+    def __init__(self, z, xy_sampling=0.05, xy_max=10.):
         """Initialize galaxy
 
         Args:
             z (float): Redshift of galaxy
-            R_e (float): Scale radius of galaxy (kpc)
 
+        Kwargs:
+            xy_sampling (float): spatial sampling of model (arcsec)
+            xy_max (float): max limit of spatial sampling (arcsec)
+
+        Raises:
+            ValueError
         """
+    
+        #Check redshift is positive
+        if z < 0.:
+            t = ("Illegal input: z (redshift) cannot be less than 0. "
+                 "Value given z={0:.3d}".format(z))
+            raise ValueError(t)
+
+        #Check spatial sampling is positive
+        if xy_sampling <= 0.:
+            t = ("Illegal input: xy_sampling (spatial sampling) cannot be less "
+                 "than 0. Value given z={0:.3d}".format(xy_sampling))
+            raise ValueError(t)
+
+        #Check spatial max limit is positive
+        if xy_max <= 0.:
+            t = ("Illegal input: xy_max (spatial limit) cannot be less "
+                 "than 0. Value given z={0:.3d}".format(xy_sampling))
+            raise ValueError(t)
+    
+        if xy_sampling >= xy_max:
+            t = ("Illegal input: xy_max should be greater than xy_sampling")
+            raise ValueError(t)
+
         self.z = z
-        self.R_e = R_e
+        self.xy_sampling = xy_sampling
+        self.xy_max = xy_max
+
+        x = np.arange(0., xy_max, xy_sampling)
+        self.x_grid = np.array([0])
+
+#        self.kpc_per_arcsec = self._kpc_per_arcsec(z)
+
+    def get_bin
--- test.py
+++ test.py
@@ -1,17 +1,62 @@
 import unittest
+import numpy as np
+from numpy import testing as npt
 
 from galaxy import Galaxy
 
 class TestGalaxy(unittest.TestCase):
 
-    def setUp(self):
-        self.galaxy = Galaxy(0.5, 15.)
+    def test_init_z(self):
+        """Redshift input validated and stored"""
+        #Is value stored
+        galaxy = Galaxy(0.5)
+        self.assertEqual(galaxy.z, 0.5)
 
-    def test_initialize_z(self):
-        self.assertEqual(self.galaxy.z, 0.5)
+        #Is z >= 0
+        with self.assertRaises(ValueError):
+            galaxy = Galaxy(-0.1)
 
-    def test_initialize_R_e(self):
-        self.assertEqual(self.galaxy.R_e, 15.)
+    def test_init_xy_sampling(self):
+        """xy spatial sampling input validated and stored"""
+        #is value stored
+        galaxy = Galaxy(1, xy_sampling=0.01)
+        self.assertEqual(galaxy.xy_sampling, 0.01)
+
+        #is xy_sampling > 0
+        with self.assertRaises(ValueError):
+            galaxy = Galaxy(1., xy_sampling=0.)
+
+    def test_init_xy_max(self):
+        """xy max spatial limit input validated and stored"""
+        #is value stored
+        galaxy = Galaxy(1, xy_max=10.)
+        self.assertEqual(galaxy.xy_max, 10.)
+
+        #is xy_max > 0
+        with self.assertRaises(ValueError):
+            galaxy = Galaxy(1., xy_max=0.)
+
+    def test_init_xy_gridding(self):
+        """xy grid constructed"""
+        #is xy_sampling < xy_max
+        with self.assertRaises(ValueError):
+            galaxy = Galaxy(1., xy_sampling=2., xy_max=1.)
+        
+        galaxy = Galaxy(1., xy_sampling=0.5, xy_max=1.5)
+
+        expect = np.array([[-0.5, 0., 0.5],
+                           [-0.5, 0., 0.5],
+                           [-0.5, 0., 0.5]])
+        npt.assert_array_equal(galaxy.x_grid, expect)
+
+        expect = np.array([[0.5, 0.5, 0.5],
+                           [0., 0., 0.],
+                           [-0.5, -0.5, -0.5]])
+        npt.assert_array_equal(galaxy.y_grid, expect)
+
+
+
+
 
 
 if __name__ == '__main__':
