c6256ec4c2eebebc8d253fbecc32f747320ca00c
David Carton <carton@strw.leidenuniv.nl>
2015-04-07 15:08:59 +0200

    remove all old

--- galaxy.py
+++ galaxy.py
@@ -1,81 +1,82 @@
 import numpy as np
+from scipy.spatial import cKDTree
+
 from cosmolopy import cd
 
-
-def Galaxy(object):
-    def __init__(self, r_max, pa, inc, n_annuli):
+class Galaxy(object):
+    def __init__(self, z, cosmo, r_max, pa, inc, n_annuli, sf_density,
+                 metallicity, lineflux):
+        self.z = z
+        self.cosmo = cosmo
 
         self._r_max = r_max
         self._pa = pa
         self._inc = inc
         self._n_annuli = n_annuli
-        self._set_bin_coords()
-    
-    #make auto update bins on parameter changes
+
+        self._set_bin_coords(r_max, pa, inc, n_annuli)
+
+        self.sf_density = sf_density
+        self.metallicity = metallicity
+        self.lineflux = lineflux
+
+#        sf_density.galaxy = self
+#        metallicity.galaxy = self
+
     @property
     def r_max(self):
         return self._r_max
 
-    @r_max.setter
-    def r_max(self, value):
-        self._r_max = value
-        self._set_bin_coords(self.r_max, self.pa, self.inc, self.n_annuli)
-
     @property
-    def r_max(self):
+    def pa(self):
         return self._pa
-
-    @pa.setter
-    def pa(self, value):
-        self._pa = value
-        self._set_bin_coords(self.r_max, self.pa, self.inc, self.n_annuli)
 
     @property
     def inc(self):
         return self._inc
 
-    @inc.setter
-    def inc(self, value):
-        self._inc = value
-        self._set_bin_coords(self.r_max, self.pa, self.inc, self.n_annuli)
-
     @property
     def n_annuli(self):
         return self._n_annuli
 
-    @n_annuli.setter
-    def n_annuli(self, value):
-        self._n_annuli = value
-        self._set_bin_coords(self.r_max, self.pa, self.inc, self.n_annuli)
 
-    def _set_coords(self, r_max, pa, inc, n_annuli):   
+    def _set_bin_coords(self, r_max, pa, inc, n_annuli):   
         x, y, radius, theta, r_in, r_out, d_theta \
             = self.calc_bin_positions(r_max, n_annuli)
 
-        x, y = self.incline_rotate(x, y, inc, pa)
-        self.x = x
-        self.y = y
+        coords = np.column_stack([x.ravel(), y.ravel()])
+        coords = self.incline_rotate(coords, inc, pa)
+
+        self.coords = coords
         self.radius = radius
         self.theta = theta
         self.radius_inner = r_in
         self.radius_outer = r_out
         self.d_theta = d_theta
+        self.area = d_theta / 2. * (r_out ** 2. - r_in ** 2.)
+
+        self.coordKDTree = cKDTree(self.coords)
 
     def calc_bin_positions(self, r_max, n_annuli):
         # radius of annuli
-        r, dr = np.linspace(0., r_max, n_annuli, retstep=True dtype=float)
+#        r, dr = np.linspace(0., r_max, n_annuli, retstep=True, dtype=float)
+        r_edge =  np.linspace(0., r_max, n_annuli+1, dtype=float)
+
+        r = (r_edge[:-1] + r_edge[1:]) / 2.
 
         #calc inner and outer edges of annuli
-        r_in = r - dr / 2.
-        r_in[0] = 0.
-        r_out = r + dr / 2.
+#        r_in = r - dr / 2.
+#        r_in[0] = 0.
+#        r_out = r + dr / 2.
+        r_in = r_edge[:-1]
+        r_out = r_edge[1:]
         
-        
-        n = 6 * np.arange(n_annuli, dtype=int) # no. bins per annulus
-        n[0] = 1
+#        n = 6 * np.arange(n_annuli, dtype=int) # no. bins per annulus
+        n = 6 * (np.arange(n_annuli, dtype=int) + 1)# no. bins per annulus
+#        n[0] = 1
 
         R = np.repeat(r, n) # bin radii
-        N = np.repeat(r, n) # number of bins at radius
+        N = np.repeat(n, n) # number of bins at radius
         r_in = np.repeat(r_in, n)
         r_out = np.repeat(r_out, n)
         # bin number within annulus
@@ -90,195 +91,57 @@ def Galaxy(object):
         return x, y, R, theta, r_in, r_out, d_theta
 
     @staticmethod
-    def incline_rotate(x, y, inc, pa):
+    def incline_rotate(coords, inc, pa):
         inc = np.radians(inc)
-        pa = np.radians(pa)
+        pa = np.radians(pa-90.)
 
-        rot_matrix = np.array([[np.cos(inc) * np.cos(pa), np.sin(pa)],
-                               [-np.cos(inc) * np.sin(pa), np.cos(pa)]])
+        rot_matrix = np.array([[np.cos(pa), np.cos(inc) * np.sin(pa)],
+                               [-np.sin(pa), np.cos(inc) * np.cos(pa)]])
 
-        coords = np.row_stack([x.ravel(), y.ravel()])
-        new_coords = np.dot(rot_matrix, coords)
-        new_x = new_coords[0].reshape(x.shape)
-        new_y = new_coords[1].reshape(y.shape)
+        new_coords = np.dot(rot_matrix, coords.T)
+        return new_coords.T
 
-        return new_x, new_y
-
-
-def create_xy_grid(x_sampling, y_sampling, x_max, y_max):
-    """Creates regualarly sampled grid centred on (0, 0)
-
-    Args:
-        x_sampling (float): spacing between samples in the x dirction
-        y_sampling (float): spacing between samples in the y dirction
-        x_max (float): defines limits of grid in x direction [-x_max, x_max]
-        y_max (float): defines limits of grid in y_dircetion [-y_max, y_max]
-
-    Returns:
-        x_grid (array): grid of x coords
-        y_grid (array): grid of y coords
-    """
-    x = np.arange(0., x_max, x_sampling)
-    x = np.concatenate([-x[::-1], x[1:]]) #make symmetric about 0
-
-    y = np.arange(0., y_max, y_sampling)
-    y = np.concatenate([-y[::-1], y[1:]]) #make symmetric about 0
-
-    x_grid, y_grid = np.meshgrid(x, y)
-    
-    return x_grid, y_grid
-
-
-class oldGalaxy(object):
-    """Galaxy model producing undegraded 2D emission line maps"""
-    def __init__(self, sfrProfile, metallicityProfile, lineFlux, lines=[],
-                 xy_sampling=0.05, xy_max=10.):
-        """Initialize galaxy
-
-        Kwargs:
-            xy_sampling (float): spatial sampling of model (arcsec)
-            xy_max (float): max limit of spatial sampling (arcsec)
-
-        Raises:
-            ValueError
-        """
-    
-        #Check spatial sampling is positive
-        if xy_sampling <= 0.:
-            t = ("Illegal input: xy_sampling (spatial sampling) cannot be less "
-                 "than 0. Value given z={0:.3d}".format(xy_sampling))
-            raise ValueError(t)
-
-        #Check spatial max limit is positive
-        if xy_max <= 0.:
-            t = ("Illegal input: xy_max (spatial limit) cannot be less "
-                 "than 0. Value given z={0:.3d}".format(xy_sampling))
-            raise ValueError(t)
-
-        if xy_sampling >= xy_max:
-           t = ("Illegal input: xy_max should be greater than xy_sampling")
-           raise ValueError(t)
-
-        #create and store store spatial grids (arcsec)
-        grids = create_xy_grid(xy_sampling, xy_sampling, xy_max, xy_max)
-        self.x_grid = grids[0]
-        self.y_grid = grids[1]
-        self.r_grid = np.sqrt(self.x_grid ** 2. + self.y_grid ** 2.)
-
-        self.x = self.x_grid.ravel()
-        self.y = self.y_grid.ravel()
-        self.r = self.r_grid.ravel()
-
-        self.pix_area = xy_sampling ** 2. #(arcsec^2)
-        
-        #store grid info (arcsec)
-        self.xy_sampling = xy_sampling
-        self.xy_max = xy_max
-        
-        self.sfrProfile = sfrProfile(self)
-        self.metallicityProfile = metallicityProfile(self)
-        self.lineFlux = lineFlux(self, lines)
-
-        self.cosmo = {'omega_M_0':0.3, 'omega_lambda_0':0.7, 'h':0.70}
-        self.cosmo = cd.set_omega_k_0(self.cosmo)
-        
-
-    def kpc_per_arcsec(self, z):
-        """Angular diameter distance in units of kpc per arcsec
-        
-        Args:
-            z - redshift
-        """
-        d_a = cd.angular_diameter_distance(z, **self.cosmo) #Mpc/rad
-        d_a *= 1e3 / (np.degrees(1.) * 3600.) #kpc/arcsec
-        return d_a
 
     def luminosity_distance(self, z):
-        """Luminosity distance in units of cm
+        """Luminosity distance in units of cm"""
+        d_lum = cd.angular_diameter_distance(self.z, **self.cosmo) #Mpc
+        d_lum *= 3.08567758e24 #cm
+        return d_lum
+
+    def model(self):
+        sf_density = self.sf_density(self.radius, self.r_max)
+        sfr = sf_density * self.area
+
+        metal = self.metallicity(self.radius)
+        np.clip(metal, 7.5, 9.5, out=metal)
+        print "CLIPPING METALLICITY"
+
+        lines = self.lineflux(sfr, metal)
+
+        lines['wave'] *= (1. + self.z)
+
+        d_lum = self.luminosity_distance(self.z)
+        norm = 4. * np.pi * d_lum ** 2.
+        lines['flux'] /= norm
+        lines['var'] /= norm ** 2
         
-        Args:
-            z - redshift
-        """
-        D_L = cd.angular_diameter_distance(z, **self.cosmo) #Mpc
-        D_L *= 3.08567758e24 #cm
-        return D_L
+        return lines
 
-    def model(self, params):
-        """Produces emission line maps of galaxy"""
-        inc = params['inc'] 
-        pa = params['PA'] 
-        
-        SFR = self.sfrProfile(params)
-        Z = self.metallicityProfile(params)
-        
-        mask = ~np.isnan(SFR)
-        SFR = SFR[mask]
-        Z = Z[mask]
-        lineflux, lines = self.lineFlux(SFR, Z, params)
-
-        x, y = self.incline_rotate(self.x[mask], self.y[mask], inc, pa)
-
-        dt = np.dtype([('x', float), ('y', float)] + lineflux.dtype.descr)
-        out = np.zeros(x.size, dtype=dt)
-        out['x'] = x
-        out['y'] = y
-        for name, _ in lineflux.dtype.descr:
-            out[name] = lineflux[name]
-
-        return out, lines
-
-     
-    @staticmethod
-    def incline_rotate(x, y, inc, pa):
-        inc = np.radians(inc)
-        pa = np.radians(pa)
-
-        rot_matrix = np.array([[np.cos(inc) * np.cos(pa), np.sin(pa)],
-                               [-np.cos(inc) * np.sin(pa), np.cos(pa)]])
-
-        coords = np.row_stack([x.ravel(), y.ravel()])
-        new_coords = np.dot(rot_matrix, coords)
-        new_x = new_coords[0].reshape(x.shape)
-        new_y = new_coords[1].reshape(y.shape)
-
-        return new_x, new_y
-
-
-    def _exponential_disc_SFR(self, z, Sigma_SFR_centre, r_s):
-        """Exponential SFR profile
-
-        Args:
-            z - redshift
-            Sigma_SFR_centre - SFR density at galaxy centre (M_sol/yr/kpc^2)
-            r_s - scale radius of disc (arcsec)
-        """
-        pix_area_kpc = self.pix_area * self.kpc_per_arcsec(z) ** 2.
-        norm = Sigma_SFR_centre * pix_area_kpc
-        profile = np.exp(-self.r_grid / r_s) #exp profile
-
-        return norm * profile # M_sol/yr
-
-    def _linear_metallicity_profile(self, r_s, Z_in, Z_out):
-        """Linear Metallicity_Profile
-
-        Args:
-            r_s - scale radius of disc (arcsec)
-            Z_in - metallicity at centre of galaxy (12 + log(O/H))
-            Z_out - metallicity at one scale radius from centre (12 + log(O/H))
-        """
-        Z = (Z_out - Z_in) * (self.r_grid / r_s) + Z_in
-        return Z
-
-
-    def _placeholder_calc_line_flux(self, Z, SFR, line):
-        #SFR(Halpha) (M_sol/yr) = 7.9x10^-42 L(Halpha) (erg/s)
-        flux = SFR / 7.9e-42 #erg/s
-        if line == 'H_BETA':
-            flux /= 2.7
-        var = flux / 10.
-        return flux, var
-    
-    #return lines, wave, pixtable(x,y,flux1,err1,flux2,err2)
+#coords class
+#    - update params simulatenously
+#    - yields k-cluster?
+#SFR class
+#    - update SFR, r_d params
+#    - call at radius, returns SFdensity
+#metallicity model class
+#    - update Z_in, Z_out
+#    - call at radius
+#line physics
+#    - call with SFR and Z
+#encpuslateing galaxy class
+#    - init with redshift, cosmology and classes
+#    - use reshift to calc flux
+#    - returns coords/k-cluster?, fluxes, vars
 
 
 #PARAMS
@@ -298,21 +161,33 @@ class oldGalaxy(object):
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
-    gal = Galaxy()
-    params = {'z': 0.5,
-              'Sigma_SFR_centre': 0.1,
-              'r_s': 1.0,
-              'inc': 70.,
-              'PA': 10.0,
-              'Z_in': 9.0,
-              'Z_out': 8.0,}
-    out = gal.model(params)
+    from SFdensity import ExpDisc
+    from metallicity import LinearMetallicity
+    from lineflux import EmpiricalLineFlux
+
+    sf_density = ExpDisc(1., 1.)
+    metallicity = LinearMetallicity(1, 8.5, 8.0)
+    filename = '/data2/MUSE/metallicity_calibration/flux_cal_singlevar.h5'
+    lineflux = EmpiricalLineFlux(filename, ['H_ALPHA', 'H_BETA'],
+                                 ['6562.', 4861.])
+
+    cosmo = {'omega_M_0' : 0.3, 'omega_lambda_0' : 0.7, 'h' : 0.7}
+    cosmo = cd.set_omega_k_0(cosmo)
+    print cosmo
+
+    galaxy = Galaxy(0.5, cosmo, 1., 10., 60., 45, sf_density, metallicity,
+                    lineflux)
 
     fig = plt.figure()
-    ax = fig.add_subplot(111, aspect='equal')
-    s = ax.hexbin(out['x'], out['y'], C=np.log10(out['H_ALPHA']), cmap='RdYlBu_r')
+    ax = fig.add_subplot(111, aspect='equal', adjustable='datalim')
+    s = ax.scatter(galaxy.coords[:,0], galaxy.coords[:,1], c=galaxy.model()[0]['flux'], s=40, cmap='RdYlBu_r')
+    s.set_edgecolor('none')
     plt.colorbar(s)
 
-    plt.show()
+#    for i in np.arange(-2, 2.1, 0.2):
+#        ax.axhline(i, color='grey', zorder=-1)
+#        ax.axvline(i, color='grey', zorder=-1)
+    
 
+    plt.show()
 
--- obssim.py
+++ obssim.py
@@ -2,17 +2,31 @@ import numpy as np
 import pyfits
 import pywcs
 from scipy.integrate import dblquad
+from scipy import sparse
+from scipy import spatial
+
 
 class ObsSim(object):
         
-    def __init__(self, ra, dec, segmap):
+    def __init__(self, ra, dec, segmap, seeing):
+        self.seeing = seeing
+        
         self.segmap, hdr = pyfits.getdata(segmap, header=True)
-
+#        self.segmap.flat = np.arange(self.segmap.size)+1
+    
         shape = self.segmap.shape + (2,)
         self.grid_centre = self.grid_centres(hdr, ra, dec).reshape(shape)
+
         shape = self.segmap.shape + (4,2)
         corners = self.grid_corners(hdr, ra, dec).reshape(shape)
         self.grid_area = self.calc_area(corners)
+
+        mask = (self.segmap >= 1)
+        self.bin_id = self.segmap[mask]
+        self.bin_centre = self.grid_centre[mask]
+        self.bin_area = self.grid_area[mask]
+
+        self.coords = spatial.cKDTree(self.bin_centre)
 
     @staticmethod
     def _dist_from(wcs, pix_x, pix_y, ra, dec):
@@ -21,7 +35,6 @@ class ObsSim(object):
         new_coords = wcs.all_pix2sky(coords, 0)
         sky_x = new_coords[:,0]
         sky_x -= np.round((sky_x - ra) / 360.) * 360.
-        print sky_x
         sky_y = new_coords[:,1]
 
         dist_x = (sky_x - ra) * np.cos(np.radians(dec)) * 3600.
@@ -67,60 +80,80 @@ class ObsSim(object):
         area = 0.5 * np.abs(AC[:,:,0] * BD[:,:,1] - BD[:,:,0] * AC[:,:,1])
         return area
    
-    @staticmethod
-    def moffat(r, a, b):
-        return (b-1.) / (np.pi*a**2.) / (1. + (r/a)**2.)**b
 
-    def model(self, data, lines):
-        a = 0.6
-        b = 2.6
+    def model(self, galaxy):
+        data = galaxy.model()
 
+
+        uniq_bin_id = np.unique(self.bin_id)
+        n_bins = len(uniq_bin_id)
+        n_pixels = len(self.bin_id)
+        area_mapper = np.zeros((n_bins, n_pixels), dtype=float)
+        for i, id_ in enumerate(uniq_bin_id):
+            mask = (self.bin_id == id_)
+            area_mapper[i][mask] = self.bin_area[mask]
+
+        area_mapper = sparse.csr_matrix(area_mapper)
+
+        dist = self.coords.sparse_distance_matrix(galaxy.coordKDTree, 3.)
+        seeing_blur = dist.tocsr()
+        seeing_blur.data[:] = self.seeing(seeing_blur.data, 7000.)
+
+        redist = area_mapper * seeing_blur
+
+        bin_flux = redist.dot(data['flux'][1])
+        
         bins = np.zeros_like(self.segmap, dtype=float)
-        seg_ids = np.unique(self.segmap)
-        for seg_id in seg_ids:
-            print seg_id
-            if seg_id <= 0:
-                continue
-            mask = (self.segmap == seg_id)
-            dx = data['x'] - self.grid_centre[mask,0][:,None]
-            dy = data['y'] - self.grid_centre[mask,1][:,None]
-            r = np.sqrt(dx**2. + dy**2.)
-
-            for line in lines:
-                frac = self.moffat(r, a, b)
-                bins[mask] = np.sum(frac * data[line['name']+'_flux'])
-        return bins
+        for i, id_ in enumerate(uniq_bin_id):
+            mask = (self.segmap == id_)
+            bins[mask] = bin_flux[i]
+#        seg_ids = np.unique(self.segmap)
+#        for seg_id in seg_ids:
+#            print seg_id
+#            if seg_id <= 0:
+#                continue
+#            mask = (self.segmap == seg_id)
+#            dx = data['x'] - self.grid_centre[mask,0][:,None]
+#            dy = data['y'] - self.grid_centre[mask,1][:,None]
+#            r = np.sqrt(dx**2. + dy**2.)
+#
+#            for line in lines:
+#                frac = self.moffat(r, a, b)
+#                bins[mask] = np.sum(frac * data[line['name']+'_flux'])
+        return bins, bin_flux
         
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    import galaxy
-    from sfr_profile import ExponentialSFRProfile
-    from metallicity_profile import LinearMetallicityProfile
-    from line_flux import PlaceholderLineFlux
 
-    lines = ['OII_3726', 'OII_3729', 'H_delta', 'H_gamma', 'H_beta',
-             'OIII_4959', 'OIII_5007']
+    from galaxy import Galaxy
+    from SFdensity import ExpDisc
+    from metallicity import LinearMetallicity
+    from lineflux import EmpiricalLineFlux
+    from seeing import MoffatSeeing
 
-    gal = galaxy.Galaxy(ExponentialSFRProfile, LinearMetallicityProfile,
-                        PlaceholderLineFlux, lines=lines)
-    
-    params = {'z': 0.5,
-              'SFRDensity_centre': 0.1,
-              'SFRDensity_thres': 1e-4,
-              'r0': 1.0,
-              'inc': 40.,
-              'PA': 30.0,
-              'Z_in': 9.0,
-              'Z_out': 8.0,}
-    data, lines = gal.model(params)
+    sf_density = ExpDisc(.1, 0.1)
+    metallicity = LinearMetallicity(1, 8.5, 8.5)
+    filename = '/data2/MUSE/metallicity_calibration/flux_cal_singlevar.h5'
+    lineflux = EmpiricalLineFlux(filename, ['OIII_5007', 'H_BETA'],
+                                 ['5007.', 4861.])
 
-    obsSim = ObsSim(180., 0., 'test_binmap3.fits')
-    im = obsSim.model(data, lines)
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect='equal')
-    im = ax.imshow(im, interpolation='nearest', origin='lower', cmap='RdYlBu_r')
+    cosmo = {'omega_M_0':0.3, 'omega_lambda_0':0.7, 'omega_k_0': 0.0, 'h':0.7}
 
-    plt.colorbar(im)
+    galaxy = Galaxy(0.564497, cosmo, 0.78042, 76.6-90., 18.94, 45,
+                    sf_density, metallicity, lineflux)
+
+    wave = np.array([4750., 7000., 9300.])
+    fwhm = np.array([0.76, 0.66, 0.61])
+    beta = np.array([2.6, 2.6, 2.6])
+    moffat = MoffatSeeing(wave, fwhm, beta)
+
+    obsSim = ObsSim(338.2239, -60.560436, '/data2/MUSE/metallicity_analysis/flux_extraction/spectra_extraction/binmap5.fits', moffat)
+    im, bin_flux = obsSim.model(galaxy)
+    print bin_flux.sum()
+    print im.sum()
+
+    plt.imshow(im, interpolation='nearest', origin='lower')
+    plt.colorbar()
     plt.show()
+    
