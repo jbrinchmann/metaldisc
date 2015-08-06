531717cfc497bf4b73b2a8f1a23515103a7459e0
David Carton <carton@strw.leidenuniv.nl>
2015-08-06 17:19:23 +0200

    document and restructure galaxy, add parent class

--- galaxy.py
+++ galaxy.py
@@ -1,118 +1,101 @@
 import numpy as np
 from scipy.spatial import cKDTree
 
-class Galaxy(object):
-    def __init__(self, z, cosmo, r_max, pa, inc, n_annuli, sf_density,
-                 metallicity, lineflux, extinction, logU_Zsolar):
-        self.__observers = []
+import utils
 
+class BaseGalaxy(object):
+
+    def __init__(self, z, cosmo, fluxgrid):
+        """Base class of galaxy models
+        
+        Parameters
+        ----------
+        z : float
+            Redshift of galaxy
+        cosmo: astropy.cosmology object
+            cosmology to use, e.g. for calculating luminosity distance
+        fluxgrid : metaldisc.fluxgrid object
+            fluxgrid object specifying the line-ratio physics
+
+        """
         self.z = z
         self.cosmo = cosmo
+        self.fluxgrid = fluxgrid
 
-        self._r_max = r_max
-        self._pa = pa
-        self._inc = inc
-        self._n_annuli = n_annuli
+        self._bin_coord = None
+        self._bin_coord_tree = None
 
-        self._set_bin_coords(r_max, pa, inc, n_annuli)
+        self.__observers = []
 
-        self.sf_density = sf_density
-        self.metallicity = metallicity
-        self.lineflux = lineflux
-        self.extinction = extinction
-        self.logU_Zsolar = logU_Zsolar
-
-
-#        sf_density.galaxy = self
-#        metallicity.galaxy = self
+        self.r_d = None # disc scale length
+        self.Z_in = None # inner metallicity
+        self.Z_out = None # metallicity at one disc scale length
+        self.tauV_in = None # inner extinction
+        self.tauV_out = None # extinction at one disc scale lenght
+        self.logU_Zsolar = None # logU at solar metallicity
 
     def register_observer(self, observer):
+        """Record objects that create views of the model"""
         self.__observers.append(observer)
     
     def notify_observers(self, *args, **kwargs):
+        """Notify objects that create views that the model has changed config"""
         for observer in self.__observers:
             observer.notify(self, *args, **kwargs)
- 
+
+    #require subclasses to have the following attributes
+    @property
+    def radius(self):
+        """Radius of galaxy bins in plane of disc [arcsec]"""
+        raise NotImplementedError("Subclasses should provide radius attribute")
 
     @property
-    def r_max(self):
-        return self._r_max
+    def theta(self):
+        """Azimuthal angle of galaxy bins in plane of disc [deg]"""
+        raise NotImplementedError("Subclasses should provide theta attribute")
 
     @property
-    def pa(self):
-        return self._pa
+    def disc_x(self):
+        """x coord of galaxy bins in plane of disc [arcsec]"""
+        raise NotImplementedError("Subclasses should provide disc_x attribute")
 
     @property
-    def inc(self):
-        return self._inc
+    def disc_y(self):
+        """y coord of galaxy bins in plane of disc [arcsec]"""
+        raise NotImplementedError("Subclasses should provide disc_y attribute")
 
     @property
-    def n_annuli(self):
-        return self._n_annuli
+    def bin_area(self):
+        """Array of galaxy sample bins areas [arcsec^2]"""
+        raise NotImplementedError("Subclasses should provide bin_area attribute")
+    
+    #require subclasses to supply bin_coords and trigger updates as required
+    @property
+    def bin_coord(self):
+        """Get Nx2 array galaxy sample coords (for N bins) [deg]"""
+        if self._bin_coord is None:
+            raise NotImplementedError("Subclasses should provide bin_coord attribute")
+        else:
+            return self._bin_coord
 
-
-    def _set_bin_coords(self, r_max, pa, inc, n_annuli):   
-        x, y, radius, theta, r_in, r_out, d_theta \
-            = self.calc_bin_positions(r_max, n_annuli)
-
-        coords = np.column_stack([x.ravel(), y.ravel()])
-        coords = self.incline_rotate(coords, inc, pa)
-
-        self.coords = coords
-        self.radius = radius
-        self.theta = theta
-        self.radius_inner = r_in
-        self.radius_outer = r_out
-        self.d_theta = d_theta
-        self.area = d_theta / 2. * (r_out ** 2. - r_in ** 2.)
-
-        self.coordKDTree = cKDTree(self.coords)
-        self.notify_observers(bins_changed=True)
-
-    def calc_bin_positions(self, r_max, n_annuli):
-        # radius of annuli
-#        r, dr = np.linspace(0., r_max, n_annuli, retstep=True, dtype=float)
-        r_edge =  np.linspace(0., r_max, n_annuli+1, dtype=float)
-
-        r = (r_edge[:-1] + r_edge[1:]) / 2.
-
-        #calc inner and outer edges of annuli
-#        r_in = r - dr / 2.
-#        r_in[0] = 0.
-#        r_out = r + dr / 2.
-        r_in = r_edge[:-1]
-        r_out = r_edge[1:]
+    #automatically compute bin_coordTree on update
+    @bin_coord.setter
+    def bin_coord(self, value):
+        """Set Nx2 array galaxy sample coords (for N bins) [deg]
         
-#        n = 6 * np.arange(n_annuli, dtype=int) # no. bins per annulus
-        n = 6 * (np.arange(n_annuli, dtype=int) + 1)# no. bins per annulus
-#        n[0] = 1
+        Forces update of cKDTree repesentation and notifies observers
+        """
+        self._bin_coord = value
+        self._bin_coord_tree = cKDTree(value) #construct cKDTree
+        self.notify_observers(bins_changed=True) #trigger notification
 
-        R = np.repeat(r, n) # bin radii
-        N = np.repeat(n, n) # number of bins at radius
-        r_in = np.repeat(r_in, n)
-        r_out = np.repeat(r_out, n)
-        # bin number within annulus
-        I = np.concatenate([np.arange(i, dtype=float) for i in n])
-
-        d_theta = 2. * np.pi / N
-        theta = I * d_theta  #angle to bin
-
-        x = R * np.cos(theta)
-        y = R * np.sin(theta)
-
-        return x, y, R, theta, r_in, r_out, d_theta
-
-    @staticmethod
-    def incline_rotate(coords, inc, pa):
-        inc = np.radians(inc)
-        pa = np.radians(pa-90.)
-
-        rot_matrix = np.array([[np.cos(pa), np.cos(inc) * np.sin(pa)],
-                               [-np.sin(pa), np.cos(inc) * np.cos(pa)]])
-
-        new_coords = np.dot(rot_matrix, coords.T)
-        return new_coords.T
-
+    @property
+    def bin_coord_tree(self):
+        """Get scipy.cKDTree representation of bin_coord"""
+        if self._bin_coordTree is None:
+            raise NotImplementedError("Subclasses should provide bin_coord attribute")
+        else:
+            return self._bin_coord_tree
 
     def luminosity_distance(self, z):
         """Luminosity distance in units of cm"""
@@ -120,37 +103,339 @@ class Galaxy(object):
         d_lum *= 3.08567758e24 #cm
         return d_lum
 
-    def model(self):
-        sf_density = self.sf_density(self.radius, self.r_max)
-        sfr = sf_density * self.area
+    def metallicity(self):
+        return
 
-        metal = self.metallicity(self.radius)
-        np.clip(metal, 7.5, 9.5, out=metal)
+    def extinction(self):
+        return
+   
 
-        tauV = self.extinction(self.radius)
-        lines = self.lineflux(sfr, metal, self.logU_Zsolar, tauV)
 
-        #ext = np.exp(-tauV * (lines['wave'][:,None]/5500.) ** -1.3)
-        #np.clip(ext, 0., 1., out=ext)
-        #lines['flux'] *= ext
-        #lines['var'] *= ext ** 2.
 
-        tauV = self.extinction(self.radius)
-        ext = np.exp(-tauV * (lines['wave'][:,None]/5500.) ** -1.3)
-        np.clip(ext, 0., 1., out=ext)
-        lines['flux'] *= ext
-        lines['var'] *= ext ** 2.
 
-        lines['wave'] *= (1. + self.z)
-
-        d_lum = self.luminosity_distance(self.z)
-        norm = 4. * np.pi * d_lum ** 2.
-        lines['flux'] /= norm
-        lines['var'] /= norm ** 2
+class GalaxyDisc(BaseGalaxy):
+    def __init__(self, ra, dec, z, pa, inc, r_max, n_annuli, cosmo, fluxgrid):  
+        """2D Galaxy Disc model
         
+        Create a galaxy disc model with a fixed specified geometry
+        Model simulates a galaxy as a set of annular segments
+        
+        Parameters
+        ----------
+        ra : float
+            Right Ascention of galaxy centre [deg]
+        dec : float
+            Declination of galaxy centre [deg]
+        z : float
+            Redshift of galaxy
+        pa : float
+            postition angle of galaxy disc [deg], North=0, East=90
+        inc : float
+            inclination of galaxy disc [deg]
+        r_max : float
+            max radius of galaxy disc [arcsec]
+        n_annuli : int
+            number of annular bins used to describe galaxy
+        cosmo: astropy.cosmology object
+            cosmology to use, e.g. for calculating luminosity distance
+        fluxgrid : metaldisc.fluxgrid object
+            fluxgrid object specifying the line-ratio physics
+        
+        """
+       
+        super(GalaxyDisc, self).__init__(z, cosmo, fluxgrid)
+
+        #protect params from being overwritten without updating geometry
+        self._ra = ra
+        self._dec = dec
+        self._pa = pa
+        self._inc = inc
+        self._r_max = r_max
+        self._n_annuli = n_annuli
+
+        self.geometry_changed = True #bins require updating
+        self._update_bin_coords() #initialize geometry
+
+        self.SFRcentre = None, #central sfr
 
 
-        return lines
+# Make sure bin coords recomputed if following geometry parameters are changed
+    #right ascention
+    @property
+    def ra(self):
+        """Get right ascention of disc"""
+        return self._ra
+
+    @ra.setter
+    def ra(self, value):
+        """Set right ascention of disc"""
+        self._ra = value
+        self.geometry_changed = True #bins require updating
+
+    #declination
+    @property
+    def dec(self):
+        """Get declination of disc"""
+        return self._dec
+
+    @dec.setter
+    def dec(self, value):
+        """Set declination of disc"""
+        self._dec = value
+        self.geometry_changed = True #bins require updating
+
+    #position angle
+    @property
+    def pa(self):
+        """Get position angle of disc"""
+        return self._pa
+
+    @pa.setter
+    def pa(self, value):
+        """Set position angle of disc"""
+        self._pa = value
+        self.geometry_changed = True #bins require updating
+
+    #inclination
+    @property
+    def inc(self):
+        """Get inclination of disc"""
+        return self._inc
+
+    @inc.setter
+    def inc(self, value):
+        """Set inclination of disc"""
+        self._inc = value
+        self.geometry_changed = True #bins require updating
+
+    #max radius
+    @property
+    def r_max(self):
+        """Get max radius of disc"""
+        return self._r_max
+
+    @r_max.setter
+    def r_max(self, value):
+        """Set max radius of disc"""
+        self._r_max = value
+        self.geometry_change = True #bins require updating
+
+    #no. annular bins
+    @property
+    def n_annuli(self):
+        """Get number of annuli in disc model"""
+        return self._n_annuli
+
+    @n_annuli.setter
+    def n_annuli(self, value):
+        """Set number of annuli in disc model"""
+        self._n_annuli = value
+        self.geometry_changed = True #bins require updating
+
+# END of geometry parameters
+
+    @staticmethod
+    def calc_bin_positions(r_max, n_annuli):
+        """Calculate annular segment bins
+        
+        Generate N=n_annuli annular between r=0 and r=r_max
+
+        Innermost bin has 6 azziumthal samples, the next has 12, then 18 etc..
+        This pattern gives the radial and transverse directions similar
+        separataion.
+
+        Parameters
+        ----------
+        r_max : float
+            max radius of disc [e.g. arcsec]
+        n_annuli : int
+            number of annular bins
+
+        Returns
+        -------
+        x : float 1D-array
+            x coord of bin centre
+        y : float 1D-array
+            y coord of bin centre
+        radius : float 1D-array
+            radial coord of bin centre
+        theta : float 1D-array
+            azimuthal coord of bin centre
+        r_in : float 1D-array
+            inner radius of bin edge
+        r_out : float 1D-array
+            outer radius of bin edge
+        d_theta: float 1D-array
+            azimuthal angle spanned by bin
+
+
+        Notes
+        -----
+        Returns a flat array of bins in sequence spiraling outwards
+
+        """
+
+        # radius of annuli
+        r_edge =  np.linspace(0., r_max, n_annuli+1, dtype=float)
+
+        # bin centre
+        r = (r_edge[:-1] + r_edge[1:]) / 2.
+
+        # inner and outer edges of annuli
+        r_in = r_edge[:-1]
+        r_out = r_edge[1:]
+        
+        n = 6 * (np.arange(n_annuli, dtype=int) + 1)# no. bins per annulus
+
+        radius = np.repeat(r, n) # bin radii
+        N = np.repeat(n, n) # number of bins at radius
+        r_in = np.repeat(r_in, n)
+        r_out = np.repeat(r_out, n)
+
+        # bin ID number within each annulus
+        I = np.concatenate([np.arange(i, dtype=float) for i in n])
+
+        d_theta = 2. * np.pi / N
+        theta = I * d_theta  #angle to bin
+
+        x = radius * np.cos(theta)
+        y = radius * np.sin(theta)
+
+        return x, y, radius, theta, r_in, r_out, d_theta
+
+
+    def transform_to_sky(self, x, y):
+        """Maps disc x and y coords to sky RA and Dec
+
+        Includes inclination and position angle effects
+
+        Parameters
+        ----------
+        x : float array
+            disc x-coords [arcsec]
+        y : float array
+            disc y-coords [arcsec]
+
+        Return
+        ------
+        ra : float array
+            right ascention [deg]
+        dec : float array
+            declination [deg]
+
+        """
+
+        coords = np.column_stack([x, y])
+                
+        inc = np.radians(self._inc)
+        pa = np.radians(90.+self._pa)
+
+        #rotate about x-axis (inc) then...
+        #rotate about z-axis (90+PA) anti-clockwise then...
+        #flip horizontal (so that East is Left)
+        rot_matrix = np.array([[-np.cos(pa), np.sin(pa)*np.cos(inc)],
+                               [np.sin(pa), np.cos(pa)*np.cos(inc)]])
+
+        new_coords = np.dot(rot_matrix, coords.T)
+
+        ra, dec = utils.separation_to_radec(new_coords[:,0], new_coords[:,1],
+                                            self._ra, self._dec)
+        return ra, dec
+
+
+    def _update_bin_coords(self):   
+        """Set bin coordinates for model sampling
+        
+        Notes
+        -----
+        Work is only done if (self.geometry_changed == True)
+
+        Return
+        ------
+        flag : bool
+            True if recomputation was performed. False if skipped
+
+        """
+
+        #only recompute if geometry of system has changed
+        if self.geometry_changed:
+
+            #calculate bin geometry
+            out = self.calc_bin_positions(self._r_max, self._n_annuli)
+            x, y, radius, theta, r_in, r_out, d_theta = out
+
+            self.disc_x = x
+            self.disc_y = y
+            self.radius = radius
+            self.theta = theta #x,y=(0,0) theta = 0 and x,y=(0,1) theta=pi/2
+            #area of annular segments
+            self.bin_area = d_theta / 2. * (r_out ** 2. - r_in ** 2.)
+
+            #calculate projected bin coords on sky
+            ra, dec = self.transform_to_sky(x, y)
+            self.bin_coords = np.column_stack([ra, dec])
+
+            #Reset flag
+            self.geometry_changed = False
+            return True
+
+        else:
+            return False
+
+    
+    def __call__(self, line, SFRcentre=None, r_d=None, Z_in=None,
+                 Z_out=None, tauV_in=None, tauV_out=None, logU_Zsolar=None):
+
+        self.SFRcentre = SFRcentre
+        self.r_d = r_d
+        self.Z_in = Z_in
+        self.Z_out = Z_out
+        self.tauV_in = tauV_in
+        self.tauV_out = tauV_out
+        self.logU_Zsolar = logU_Zsolar
+        
+        #get Z of bins
+        Z = self.metallicity()
+        #get logU of bins
+        logU = self.ionization_parameter()
+        #get line flux
+        self.fluxgrid(line, Z, logU)
+        #multiply SFR of bins
+        SFR = self.SFR()
+        #multiply tauV of bins
+        flux = self.extinction(line, flux, var)
+
+        return flux, var
+
+#    def model(self):
+#        sf_density = self.sf_density(self.radius, self.r_max)
+#        sfr = sf_density * self.area
+#
+#        metal = self.metallicity(self.radius)
+#        np.clip(metal, 7.5, 9.5, out=metal)
+#
+#        tauV = self.extinction(self.radius)
+#        lines = self.lineflux(sfr, metal, self.logU_Zsolar, tauV)
+#
+#        #ext = np.exp(-tauV * (lines['wave'][:,None]/5500.) ** -1.3)
+#        #np.clip(ext, 0., 1., out=ext)
+#        #lines['flux'] *= ext
+#        #lines['var'] *= ext ** 2.
+#
+#        tauV = self.extinction(self.radius)
+#        ext = np.exp(-tauV * (lines['wave'][:,None]/5500.) ** -1.3)
+#        np.clip(ext, 0., 1., out=ext)
+#        lines['flux'] *= ext
+#        lines['var'] *= ext ** 2.
+#
+#        lines['wave'] *= (1. + self.z)
+#
+#        d_lum = self.luminosity_distance(self.z)
+#        norm = 4. * np.pi * d_lum ** 2.
+#        lines['flux'] /= norm
+#        lines['var'] /= norm ** 2
+#
+#        return lines
 
 #coords class
 #    - update params simulatenously
