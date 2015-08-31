08c8d3be1c3f929304c7dc545beff292415f9f58
David Carton <cartondj@gmail.com>
2015-08-31 20:54:36 +0200

    Docstrings added

--- obssim.py
+++ obssim.py
@@ -1,14 +1,9 @@
 import numpy as np
-from astropy import wcs
 from scipy import sparse
 from scipy.spatial import cKDTree
+from astropy import wcs
 
-#base obs sim with mapping code
-#make image from binmap
-#make image from spec ra dec, pixsize, etc.
     
-#restore notify and register observers
-
 class BaseObsSim(object):
 
     def __init__(self, galaxy, seeing, conserve_flux=False):
@@ -103,7 +98,11 @@ class BaseObsSim(object):
 
 
     def _calc_pixel2bin_matrix(self):
-        """Constructs matrix mapping pixel area to bins
+        """Constructs matrix mapping pixel to bins mulitplied by pixel area
+
+        Notes
+        -----
+        if self.conserving_flux=False then bins are normalized to number of pixels contributing to the bin
 
         Returns
         -------
@@ -127,6 +126,20 @@ class BaseObsSim(object):
         return m
 
     def _calc_psf2pixel_matrix(self, wave):
+        """Constructs matrix mapping galaxy sample elements image pixels
+
+        Calculates element -> pixel distances and computes PSF for each distance
+
+        Parameters
+        ----------
+        wave : float
+            wavelength [Angstrom] at which PSF is computed
+
+        Returns
+        -------
+        m : CSR sparse matrix
+
+        """
         maxdist = self.seeing.radius_enclosing(0.995, wave)
 
         #get distance between image pixels and galaxy bins
@@ -141,7 +154,18 @@ class BaseObsSim(object):
     
 
     def calc_mapping_matrix(self, line):
+        """Constructs matrix mapping galaxy sample elements output bins
+        
+        Parameters
+        ----------
+        line : string
+            emission line identification
 
+        Returns
+        -------
+        m : CSR sparse matrix
+
+        """
         wave = self.galaxy.get_obs_wave(line) #observed wavelength
 
         #matrix mapping image pixels to image bins multiplied by pixel area
@@ -156,7 +180,20 @@ class BaseObsSim(object):
 
 
     def get_mapping_matrix(self, line):
+        """Returns a matrix mapping galaxy sample elements output bins
 
+        Stores matrix for future iterations.
+        
+        Parameters
+        ----------
+        line : string
+            emission line identification
+
+        Returns
+        -------
+        m : CSR sparse matrix
+
+        """
         try:
             #check if already computed
             m = self.mapping_matrix[line]
@@ -168,6 +205,22 @@ class BaseObsSim(object):
         return m
 
     def __call__(self, lines, params):
+        """Calculate line fluxes and variances for a set of emission lines
+        
+        Parameters
+        ----------
+        lines : list of strings
+            names identifying emission lines
+        params : dict
+            dictionary of model parameters
+
+        Returns:
+        flux : array of floats, shape:(a,b)
+            emission line fluxes, a:#bins b:#lines [erg/s/cm^2]
+        var : array of floats, shape:(a,b)
+            corresponding variances
+
+        """
 
         uniq_pixel_id = np.unique(self.pixel_id)
         n_bins = len(uniq_pixel_id)
@@ -328,141 +381,6 @@ class BinmapObsSim(BaseObsSim):
 
         return area
 
-
-#class ObsSim(object):
-#        
-#    def __init__(self, galaxy, ra, dec, segmap, seeing):
-#        self.galaxy = galaxy
-#
-#        self.seeing = seeing
-#        
-#        self.segmap, hdr = pyfits.getdata(segmap, header=True)
-##        self.segmap.flat = np.arange(self.segmap.size)+1
-#    
-#        shape = self.segmap.shape + (2,)
-#        self.grid_centre = self.grid_centres(hdr, ra, dec).reshape(shape)
-#
-#        shape = self.segmap.shape + (4,2)
-#        corners = self.grid_corners(hdr, ra, dec).reshape(shape)
-#        self.grid_area = self.calc_area(corners)
-#
-#        mask = (self.segmap >= 1)
-#        self.bin_id = self.segmap[mask]
-#        self.bin_centre = self.grid_centre[mask]
-#        self.bin_area = self.grid_area[mask]
-#
-#        self.coords = spatial.cKDTree(self.bin_centre)
-#        self.create_bins_to_samples_area_map()
-#        self.calc_samples_to_points_dist()
-#
-#        self.redist = {}
-# 
-#
-#    @staticmethod
-#    def _dist_from(wcs, pix_x, pix_y, ra, dec):
-#        coords = np.column_stack([pix_x, pix_y])
-#
-#        new_coords = wcs.all_pix2sky(coords, 0)
-#        sky_x = new_coords[:,0]
-#        sky_x -= np.round((sky_x - ra) / 360.) * 360.
-#        sky_y = new_coords[:,1]
-#
-#        dist_x = (sky_x - ra) * np.cos(np.radians(dec)) * 3600.
-#        dist_y = (sky_y - dec) * 3600.
-#
-#        return dist_x, dist_y 
-#
-#    def grid_centres(self, hdr, ra, dec):
-#        n_x = hdr['NAXIS1']
-#        n_y = hdr['NAXIS2']
-#        wcs = pywcs.WCS(hdr)
-#
-#        x = np.tile(np.arange(n_x, dtype=float), n_y).ravel()
-#        y = np.repeat(np.arange(n_y, dtype=float), n_x).ravel()
-#
-#        x, y = self._dist_from(wcs, x, y, ra, dec)
-#        centres = np.column_stack([x, y])
-#        return centres
-#
-#    def grid_corners(self, hdr, ra, dec):
-#        n_x = hdr['NAXIS1']
-#        n_y = hdr['NAXIS2']
-#        wcs = pywcs.WCS(hdr)
-#
-#        x = np.tile(np.arange(n_x, dtype=float), n_y).ravel()
-#        y = np.repeat(np.arange(n_y, dtype=float), n_x).ravel()
-#        
-#        corners = np.zeros((n_x*n_y,4,2), dtype=float)
-#        corners[:,0,0], corners[:,0,1] = self._dist_from(wcs, x-0.5, y-0.5,
-#                                                         ra, dec)
-#        corners[:,1,0], corners[:,1,1] = self._dist_from(wcs, x+0.5, y-0.5,
-#                                                         ra, dec)
-#        corners[:,2,0], corners[:,2,1] = self._dist_from(wcs, x-0.5, y+0.5,
-#                                                         ra, dec)
-#        corners[:,3,0], corners[:,3,1] = self._dist_from(wcs, x+0.5, y+0.5,
-#                                                         ra, dec)
-#        return corners
-#
-#    @staticmethod
-#    def calc_area(corners):
-#        AC = corners[:,:,3,:] - corners[:,:,0,:]
-#        BD = corners[:,:,2,:] - corners[:,:,1,:]
-#        area = 0.5 * np.abs(AC[:,:,0] * BD[:,:,1] - BD[:,:,0] * AC[:,:,1])
-#        return area
-#
-#    def create_bins_to_samples_area_map(self):
-#        #construct map from bins to sample points - do this once
-#        uniq_bin_id = np.unique(self.bin_id)
-#        n_bins = len(uniq_bin_id)
-#        n_pixels = len(self.bin_id)
-#        area_mapper = np.zeros((n_bins, n_pixels), dtype=float)
-#        for i, id_ in enumerate(uniq_bin_id):
-#            mask = (self.bin_id == id_)
-#            area_mapper[i][mask] = self.bin_area[mask] / np.sum(mask)
-#
-#        self.area_mapper = sparse.csr_matrix(area_mapper)
-#
-#    def calc_samples_to_points_dist(self):
-#        #calc dist from sample cooords to galaxy coords (less that 3")
-#        dist = self.coords.sparse_distance_matrix(self.galaxy.coordKDTree, 3.)
-#        self.dist_matrix = dist.tocsr()
-#   
-#
-#    def model(self):
-#        data = self.galaxy.model()
-#
-#        uniq_bin_id = np.unique(self.bin_id)
-#        out_dt = np.dtype(data.dtype.descr[0:2] + 
-#                          [('flux', float, uniq_bin_id.size),
-#                           ('var', float, uniq_bin_id.size)])
-#        out = np.zeros(len(data), dtype=out_dt)
-#
-#        for i in xrange(len(data)):
-#            line = data[i]['line']
-#            wave = data[i]['wave']
-#
-#            out['line'][i] = line
-#            out['wave'][i] = wave
-#
-#            try:
-#                redist_flux, redist_var = self.redist[line]
-#
-#            except KeyError:
-#                seeing_redist = self.dist_matrix.copy()
-#
-#                seeing_redist.data[:] = self.seeing(seeing_redist.data, wave)
-#                redist_flux = self.area_mapper * seeing_redist
-#
-#                redist_var = redist_flux.copy()
-#                redist_var.data[:] = redist_var.data[:] ** 2.
-#
-#                self.redist[line] = (redist_flux, redist_var)
-#
-#            out['flux'][i] = redist_flux.dot(data[i]['flux'])
-#            out['var'][i] = redist_var.dot(data[i]['var'])
-#
-#        return out
-        
 
 if __name__ == '__main__':
     from astropy.io import fits
