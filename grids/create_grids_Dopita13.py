f38528980a70f12c147ac05f403a365c73a0d205
David Carton <carton@strw.leidenuniv.nl>
2016-04-18 21:03:40 +0200

    extend D13 models for H_delta and epsilon

--- grids/create_grids_Dopita13.py
+++ grids/create_grids_Dopita13.py
@@ -2,7 +2,41 @@ import numpy as np
 from scipy.interpolate import LinearNDInterpolator
 import h5py
 from astropy.table import Table
+import pyneb as pn
 
+line_wavelengths = {
+    'O2-3727': 3727.092,
+    'O2-3729': 3729.875,
+    'Ne3-3869': 3870.16,
+    'He': 3970.072,
+    'S2-4068': 4073.6245, #blend
+    'Hd': 4101.734,
+    'Hg': 4341.684,
+    'O3-4363': 4364.436,
+    'He1-4471': 4472.767, #blend
+    'Hb': 4862.683,
+    'O3-4959': 4960.295,
+    'O3-5007': 5008.240,
+    'He1-5016': 5017.0765,
+    'Ar3-5192': 5193.27,
+    'N1-5198': 5200.527, #blend
+    'N2-5755': 5756.240,
+    'He1-5875': 5877.305, #blend
+    'O1-6300': 6302.046,
+    'S3-6313': 6313.8,
+    'N2-6548': 6549.85,
+    'Ha': 6564.61,
+    'N2-6584': 6585.28,
+    'He1-6678': 6679.99656,
+    'S2-6717': 6718.29,
+    'S2-6731': 6732.67,
+    'Ar3-7136': 7137.8,
+    'O2-7318': 7326.845, #blend
+    'Ar3-7751': 7753.2,
+    'S3-9068': 9071.1,
+    'S3-9532': 9533.2,
+    }
+#line centres in vacuum http://www.pa.uky.edu/~peter/atomic/ v2.04
 
 
 
@@ -45,7 +79,7 @@ def load_data(kappa):
 
     lines_blue = data_blue.colnames[4:]
     lines_red = data_red.colnames[4:]
-    lines = lines_blue + lines_red
+    lines = np.array(lines_blue + lines_red)
 
     Z = data_blue[mask_kappa]['Z'].data
     logq = data_blue[mask_kappa]['logq'].data
@@ -57,15 +91,65 @@ def load_data(kappa):
 
     flux = np.column_stack([flux_blue, flux_red])
 
+    #sort lines (should already be)
+    wave = [line_wavelengths[l] for l in lines]
+    sort = np.argsort(wave)
+    lines = lines[sort]
+    flux = flux[:,sort]
+
     return Z, logq, lines, flux
 
 
-def rescale_flux(flux, lines):
+def interp_caseb(flux_Hb, flux_Hg, line):
+
+    temp = np.logspace(np.log10(500.), np.log10(30000.), 10000)[1:-1]
+    den = 10.
+
+    H1 = pn.RecAtom('H', 1)
+    model_Hb = H1.getEmissivity(temp, den, 4, 2)
+    model_Hg = H1.getEmissivity(temp, den, 5, 2)
+
+    if line == 'Hd':
+        model_line = H1.getEmissivity(temp, den, 6, 2)
+    elif line == 'He':
+        model_line = H1.getEmissivity(temp, den, 7, 2)
+    else:
+        raise Exception
+
+    flux_HgHb = flux_Hg / flux_Hb
+    model_HgHb = model_Hg / model_Hb
+    model_lineHb = model_line / model_Hb
+    flux_lineHb = np.interp(flux_HgHb, model_HgHb, model_lineHb)
+    flux_line = flux_lineHb * flux_Hb
+
+    return flux_line
+
+
+def extend_hydrogen(lines, flux):
+
+    for balmer_line in ['Hd', 'He']:
+        flux_Hb = flux[:,lines=='Hb']
+        flux_Hg = flux[:,lines=='Hg']
+        
+        flux_line = interp_caseb(flux_Hb, flux_Hg, balmer_line)
+
+        wave = [line_wavelengths[l] for l in lines]
+        balmer_wave = line_wavelengths[balmer_line]
+        idx = np.searchsorted(wave, balmer_wave)
+
+        lines = np.concatenate([lines[:idx], [balmer_line], lines[idx:]])
+        flux = np.column_stack([flux[:,:idx], flux_line, flux[:,idx:]])
+
+    return lines, flux
+
+
+
+def rescale_flux(lines, flux):
 
     SFR = 1. # M_sun / yr
     Ha_expected = SFR / 7.9e-42 # erg/s 1998ARA&A..36..189K
 
-    Ha_obs = flux[:,np.array(lines) == 'Ha']
+    Ha_obs = flux[:,lines=='Ha']
     norm = Ha_obs / Ha_expected
 
     flux /= norm
@@ -89,39 +173,8 @@ def create_grid(logZ, logU, flux):
 
 
 def create_file(filename, logZ, logU, lines, flux):
-    wave_lookup = {
-        'O2-3727': 3727.092,
-        'O2-3729': 3729.875,
-        'Ne3-3869': 3870.16,
-        'S2-4068': 4073.6245, #blend
-        'Hg': 4341.684,
-        'O3-4363': 4364.436,
-        'He1-4471': 4472.767, #blend
-        'Hb': 4862.683,
-        'O3-4959': 4960.295,
-        'O3-5007': 5008.240,
-        'He1-5016': 5017.0765,
-        'Ar3-5192': 5193.27,
-        'N1-5198': 5200.527, #blend
-        'N2-5755': 5756.240,
-        'He1-5875': 5877.305, #blend
-        'O1-6300': 6302.046,
-        'S3-6313': 6313.8,
-        'N2-6548': 6549.85,
-        'Ha': 6564.61,
-        'N2-6584': 6585.28,
-        'He1-6678': 6679.99656,
-        'S2-6717': 6718.29,
-        'S2-6731': 6732.67,
-        'Ar3-7136': 7137.8,
-        'O2-7318': 7326.845, #blend
-        'Ar3-7751': 7753.2,
-        'S3-9068': 9071.1,
-        'S3-9532': 9533.2,
-        }
-    #line centres in vacuum http://www.pa.uky.edu/~peter/atomic/ v2.04
 
-    wave = [wave_lookup[l] for l in lines]
+    wave = [line_wavelengths[l] for l in lines]
     
     fh = h5py.File(filename, 'w')
 
@@ -150,16 +203,19 @@ def create_file(filename, logZ, logU, lines, flux):
     return fh
 
 if __name__ == '__main__':
-
+    
     for kappa in [10, 20, 50, 'inf']:
         Z, logq, lines, flux = load_data(kappa)
+        #add Hd and He
+        lines, flux = extend_hydrogen(lines, flux)
+        
 
         logZ = np.log10(Z)
 
         cspeed = 29979245800. #speed of light in cm
         logU = logq - np.log10(cspeed)
         
-        rescale_flux(flux, lines)
+        rescale_flux(lines, flux)
         
         logZ, logU, flux = create_grid(logZ, logU, flux)
 
