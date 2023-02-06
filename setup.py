1f03a38fcf4b7daa374ce3a28a13c6421d84aaea
Jarle Brinchmann <jbrinchmann@gmail.com>
2023-02-06 09:50:46 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='metaldisc',
+      version='1.0',
+      install_requires=['numpy', 'scipy', 'pymultinest', 'h5py', 'astropy'],
+      )
+
